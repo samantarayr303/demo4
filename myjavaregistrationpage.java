5d86121dfc1b11e3c1756c6883d6ff2ff5ad8be1
lucky <samantarayr303@gmail.com>
2023-03-10 00:40:43 +0530

    my pending task

--- myjavaregistrationpage.java
+++ myjavaregistrationpage.java
@@ -1,3 +1,11 @@
 This is my java reg page
+<<<<<<< Updated upstream
 This is manager work
+=======
+1st day of java code
+2nd day of java code
+3rd day of java code
+4th day of java code
+5th day of java code
+>>>>>>> Stashed changes
 
